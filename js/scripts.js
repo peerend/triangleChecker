a10a10882eff5904a7b7e58adeb7452a873b2fd4
Christian Danielsen & Dylan Peerenboom <student@epicodus.com>
2014-07-16 16:45:29 -0700

    html working: NaN and impossible triangle bugs present

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,39 @@
-function triangleTracker(side1, side2, side3){
-
-  var sortedSides = [side1, side2, side3].sort(function(a, b) {
-    return a - b;
-  });
-  var shortSide = sortedSides[0];
-  var longSide = sortedSides[1];
-  var hypotenuse = sortedSides[2];
-
-  if (shortSide + longSide < hypotenuse)
-    return("impossible");
-
-  var triangle = {
-  shortSide : shortSide,
-  longSide : longSide,
-  hypotenuse : hypotenuse,
+var triangle = {
   triangleType : function() {
+    var sortedSides = [this.side1, this.side2, this.side3].sort(function(a, b) {
+      return a - b;
+    });
+
+    this.shortSide = sortedSides[0];
+    this.longSide = sortedSides[1];
+    this.hypotenuse = sortedSides[2];
+
     if (this.shortSide === this.longSide && this.longSide === this.hypotenuse){
       return("equilateral");
     }else if (this.shortSide === this.longSide || this.longSide === this.hypotenuse || this.shortSide === this.hypotenuse){
       return("isosceles");
-    }else{
+    }else if (this.shortSide !== this.longSide && this.shortSide !== this.hypotenuse ){
       return('scalene');
+    }else {
+      return("impossible");
     }
   }
-  }
-
-  return [triangle.triangleType(), [triangle.shortSide, triangle.longSide, triangle.hypotenuse]];
 };
 
 $(document).ready(function(){
   $("form.user_input").submit(function(event){
-     var side1 = parseInt($("#side1").val());
-     var side2 = parseInt($("#side2").val());
-     var side3 = parseInt($("#side3").val());
-     var output = triangleTracker(side1, side2, side3);
-    console.log(output[0]);
-    if (output[0] === 'equilateral'){
-      $("#equilateral").append("<li>" + output[1] + "</li>");
-    } else if (output[0] === 'isosceles'){
-        $("#isosceles").append("<li>" + output[1] + "</li>");
-    } else if (output[0] === 'scalene'){
-        $("#scalene").append("<li>" + output[1] + "</li>");
+     triangle.side1 = parseInt($("#side1").val());
+     triangle.side2 = parseInt($("#side2").val());
+     triangle.side3 = parseInt($("#side3").val());
+     var output = triangle.triangleType();
+     console.log(triangle.triangleType());
+     console.log(output);
+    if (output === 'equilateral'){
+      $("#equilateral").append("<li>" + triangle.shortSide +', '+ triangle.longSide +', '+ triangle.hypotenuse  + "</li>");
+    } else if (output === 'isosceles'){
+        $("#isosceles").append("<li>"+ triangle.shortSide +', '+ triangle.longSide +', '+ triangle.hypotenuse  + "</li>");
+    } else if (output === 'scalene'){
+        $("#scalene").append("<li>"+ triangle.shortSide +', '+ triangle.longSide +', '+ triangle.hypotenuse  + "</li>");
     } else {
         alert(output);
     }
--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,15 @@
 
-describe ("triangleTracker", function(){
+describe ("triangleType", function(){
   it("Given two lengths whose sum is less than the third, will classify the triangle as impossible", function(){
-  	triangleTracker(10,1,4).should.equal("impossible");
+  	triangleType(10,1,4).should.equal("impossible");
   });
   it("Given three lengths that are all equal, will classify the triangle as equilateral and return its lengths", function(){
-    triangleTracker(3,3,3).should.eql(['equilateral', [3, 3, 3]]);
+    triangleType(3,3,3).should.eql(['equilateral', [3, 3, 3]]);
   });
     it("Given no lengths that are equal, will classify the triangle as scalene and return its lengths", function(){
-    triangleTracker(3,5,4).should.eql(['scalene', [3, 4, 5]]);
+    triangleType(3,5,4).should.eql(['scalene', [3, 4, 5]]);
   });
     it("Given two lengths that are equal, will classify the triangle as isosceles and return its lengths", function(){
-    triangleTracker(3,4,4).should.eql(['isosceles', [3, 4, 4]]);
+    triangleType(3,4,4).should.eql(['isosceles', [3, 4, 4]]);
   });
-
 });
